80689605739954608113492b1cc3c0e94376fab3
leo-g1864 <lpcgarciaa@gmail.com>
2026-07-16 06:18:37 +0000

    Tasks/Task1.java

---
+++ Tasks/Task1.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task1 {
+
+}
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
