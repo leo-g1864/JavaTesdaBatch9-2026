a98a461bb9f943f09db32049348eafe28a6c4638
leo-g1864 <lpcgarciaa@gmail.com>
2026-08-09 15:27:03 +0000

    Declare all Java primitive data types (excluding long and double), assign each a different value, then concatenate them into a single String and print the result to standard output.

---
+++ primitives.java
@@ -0,0 +1,21 @@
+public class primitives {
+    public static void main(String[] args) {
+
+        byte zero = 0;
+        short zeroo = 48;
+        int three = 3;
+        char h = 'H';
+        char l = 'l';
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        float tupointow = 2f;
+        boolean istrulalu = true;
+
+        String outcome = "" + h + three + l + l + zero
+                + " " + w + (char) zeroo + r + l + d + " "
+                + tupointow + " " + istrulalu;
+
+        System.out.println(outcome);
+    }
+}
